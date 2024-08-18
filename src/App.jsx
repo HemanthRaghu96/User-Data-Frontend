bf42b4ee0b4ac2563696e0c17cb8595dbb4c0537
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-08-18 16:56:29 +0530

    Created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import UserList from "./pages/UserList/UserList";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
-
-   </>
-  )
+   
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+   
+  );
 }
 
-export default App
+export default App;
